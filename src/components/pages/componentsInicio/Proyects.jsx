a7b5a5e93a462f6abc8b5e28008f2e7d823b5d98
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-06-22 03:07:52 -0300

    se creo la seccion de skills

---
+++ src/components/pages/componentsInicio/Proyects.jsx
@@ -0,0 +1,279 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Bistro from "../../../assets/bistro.png";
+import ListaTareas from "../../../assets/listaTareas.png";
+import Coffee from "../../../assets/coffee.png";
+import Paleta from "../../../assets/paleta.png";
+import Clima from "../../../assets/clima.png";
+import Noticias from "../../../assets/noticias.png";
+import Peliculas from "../../../assets/peliculas.png";
+import Resetas from "../../../assets/resetas.png";
+import Parrillada from "../../../assets/parrillada.png";
+import Simpsons from "../../../assets/simpsons.png";
+import Agenda from "../../../assets/agenda.png";
+
+const Proyects = () => {
+  return (
+    <section className="proyectos">
+      <h2 className="text-white display-5 my-3 text-center mb-5">PROYECTOS</h2>
+      <Container>
+        <Row>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Bistro}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={ListaTareas}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Coffee}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Paleta}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Clima}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Noticias}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Peliculas}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Resetas}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Parrillada}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Simpsons}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={4} className="peliculas">
+            <div className="cardProyecto">
+              <div className="pelicula">
+                <img
+                  className="imgTendencia"
+                  src={Agenda}
+                  alt=""
+                  loading="lazy"
+                />
+
+                <div className="tituloPelicula ">
+                  <div className="menuProyecto d-flex justify-content-center">
+                    <Button variant="primary">
+                      <i className="bi bi-github"></i>
+                    </Button>
+                    <Button variant="dark" className="ms-2">
+                      <i className="bi bi-box-arrow-up-right"></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Proyects;
---
+++ src/components/pages/componentsInicio/Skills.jsx
@@ -0,0 +1,257 @@
+import { Col, Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Html from '../../../assets/html.png';
+import Css from '../../../assets/css.png';
+import Bootstrap from '../../../assets/bootstrap.jfif';
+import Vue from '../../../assets/vue.png';
+import React from '../../../assets/react.png';
+import Node from '../../../assets/nodejs.png';
+import Express from '../../../assets/express.png';
+import Mongo from '../../../assets/mongo.webp';
+import Java from '../../../assets/java.webp';
+import C from '../../../assets/c.webp';
+import Cpp from '../../../assets/c++.png';
+import Python from '../../../assets/python.webp';
+import Mysql from '../../../assets/mysql.webp';
+import Ruby from '../../../assets/ruby1.webp';
+
+const Skills = () => {
+  return (
+    <section className="habilidades py-3">
+      <Container>
+        <h2 className="text-white display-3 d-flex justify-content-center">
+          Habilidades
+        </h2>
+        <Row className="d-flex">
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Html}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill ">
+              <img
+                src={Css}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s"
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Bootstrap}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Vue}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>        
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={React}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Node}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Express}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Mongo}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Java}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={C}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Cpp}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Python}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Mysql}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+          <Col
+            lg={2}
+            md={3}
+            sm={6}
+            xs={6}
+            className="d-flex justify-content-center"
+          >
+            <div className="skill">
+              <img
+                src={Ruby}
+                alt=""
+                className="w-100"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Skills;
